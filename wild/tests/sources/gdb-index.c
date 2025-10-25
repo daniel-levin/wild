dc63d40c5057b298cf5edcd2e0ecf4a00d6195e3
Daniel Levin <daniellevin2607@gmail.com>
2025-10-25 12:38:09 +0000

    made section required

--- wild/tests/sources/gdb-index.c
+++ wild/tests/sources/gdb-index.c
@@ -1,5 +1,16 @@
 //#Config:clang
 //#Compiler: clang
+//#LinkerDriver:clang
+//#LinkArgs:-z now -Wl,--gdb-index
+//#SkipLinker:ld
+//#EnableLinker:lld
+//#DiffIgnore:section.relro_padding
+//#DiffIgnore:section.got.plt.entsize
+//#DiffIgnore:section.rodata
+//#DiffIgnore:section.gnu.version_r.alignment
+//#Contains:gdb_index
+
+//#Compiler: clang
 //#CompArgs:-ggdb
 //#LinkerDriver:clang
 //#LinkArgs:-z now -Wl,--gdb-index
