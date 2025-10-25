69aa913607256bd4556898a313e819e16a756b98
Daniel Levin <daniellevin2607@gmail.com>
2025-10-25 12:31:20 +0000

    Ignore irrelevant parts of diff

--- wild/tests/sources/gdb-index.c
+++ wild/tests/sources/gdb-index.c
@@ -1,8 +1,13 @@
 //#Config:clang
 //#Compiler: clang
+//#CompArgs:-ggdb
 //#LinkerDriver:clang
 //#LinkArgs:-z now -Wl,--gdb-index
 //#SkipLinker:ld
 //#EnableLinker:lld
+//#DiffIgnore:section.relro_padding
+//#DiffIgnore:section.got.plt.entsize
+//#DiffIgnore:section.rodata
+//#DiffIgnore:section.gnu.version_r.alignment
 
 int main() { return 42; }
